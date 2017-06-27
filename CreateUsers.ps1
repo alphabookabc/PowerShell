a405b9dc418869f4cdf701f9675aacff0ea01e88
Alpha Knowledge <shenhuitao2000@163.com>
2017-06-27 23:23:29 +0800

    Update CreateUsers.ps1

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -1,4 +1,18 @@
 <#
 Import users from users.csv, the create users in AD.
 #>
+Function New-CorpUser {
 
+  Param (
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$FirstName,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$LasttName,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$Title,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$Department,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$City,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$Country,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$FirstName,
+  [Parameter(Mandatory=$True,ValueFromPiplineByPropertyName=$True)][Sting]$FirstName,
+  )
+
+
+} # Function
